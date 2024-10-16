7429a9a0bff1682a1c969963956a3cb2b3ed9afb
David <davidkliba728@gmail.com>
2024-10-16 13:11:24 +0200

    Script Usuarios

---
+++ script.ps1
@@ -0,0 +1,5 @@
+﻿New-Item  -ItemType Directory -Path C:\ -Name Directorio1
+Set-Location -Path C:\Directorio1 
+New-Item -ItemType File -Name hola.txt
+Add-Content -Path hola.txt David  
+Copy-Item hola.txt hola.bak
